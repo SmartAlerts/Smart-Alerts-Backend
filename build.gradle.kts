3872cd023bcc957bbc7d4dceac0b6767d424a830
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-04-20 19:36:58 -0400

    🗃️ Add SQLDelight and initial model

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   alias(libs.plugins.kotlin)
   alias(libs.plugins.ktor)
   alias(libs.plugins.kotlinx.serialization)
+  alias(libs.plugins.sqldelight)
 }
 
 group = "app.smartalerts"
@@ -32,4 +33,14 @@ dependencies {
 
 tasks.withType<Test> {
   useJUnitPlatform()
+}
+
+sqldelight {
+  databases {
+    create("Database") {
+      packageName.set("app.smartalerts")
+      dialect("app.cash.sqldelight:mysql-dialect:${libs.versions.sqldelight.get()}")
+      deriveSchemaFromMigrations.set(true)
+    }
+  }
 }
